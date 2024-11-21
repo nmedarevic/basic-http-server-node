9d6b514004c0cbc26211dea8eff16ff363be0b33
Nikola Medarevic <nikola@petmediagroup.com>
2024-11-21 23:02:24 +0100

    Enable eslint and prettier

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,29 @@
-import 'dotenv/config'
+import 'dotenv/config';
 import express, { Request, Response } from 'express';
 
 async function main() {
   const app = express();
   const port = process.env.PORT;
 
-  if (typeof port === "undefined") {
-    throw new Error("Server port is not defined!");
+  if (typeof port === 'undefined') {
+    throw new Error('Server port is not defined!');
   }
- 
+
   app.get('/', (_: Request, res: Response) => {
-    res.send('Hello World!'); 
+    res.send('Hello World!');
   });
 
   const server = app.listen(port, () => {
     console.log(`Server started at port ${port}`);
   });
 
-  process.on("SIGTERM", () => {
-    console.log('SIGTERM signal received: closing HTTP server')
+  process.on('SIGTERM', () => {
+    console.log('SIGTERM signal received: closing HTTP server');
 
     server.close(() => {
-      console.log('HTTP server closed')
-    })
-  })
+      console.log('HTTP server closed');
+    });
+  });
 }
-
 
 void main();
